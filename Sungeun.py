9b74bf57fe9470157900048110e5faf5c6e2f32f
Sparrow Coding <163827077+SparrowCoding@users.noreply.github.com>
2024-05-13 10:29:21 +0900

    Homework_file

--- Sungeun.py
+++ Sungeun.py
@@ -1,1 +1,10 @@
-print("dog_foot_bird_foot_")
+S = input("이름을 입력하세요 :")
+print("그래 과제 켰구나 "+ S+"아")
+a = int(input("오늘 날짜를 입력하시오 ex 5월 30일= 530 =="))
+
+if a <= 522:
+    print("적당한 시기에 과제를 마쳤네용")
+elif a > 522 and a < 526:
+    print("좀 늦게 과제를 마쳤네요 다음부턴 서둘러주세요")
+else:
+    print("과제가 너무 늦게 끝났는데용")
